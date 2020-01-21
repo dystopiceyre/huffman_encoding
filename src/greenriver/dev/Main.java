bfd34592cd959d58aa164ec25b8acb8cf0e8726e
dystopiceyre <livvy16.olivia@gmail.com>
2020-01-20 23:59:43 -0800

    Add Huffman Nodes to a priority queue

--- src/greenriver/dev/Main.java
+++ src/greenriver/dev/Main.java
@@ -1,6 +1,7 @@
 package greenriver.dev;
 
 import java.util.Map;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class Main {
@@ -14,21 +15,19 @@ public class Main {
         //Ask for string input
         Scanner scanner = new Scanner(System.in);
         FrequencyCounter freqCounter = new FrequencyCounter();
-        HuffmanNode huffmanNode = new HuffmanNode();
         System.out.println("Welcome to my Huffman Encoder! Please enter a string to be encoded below.");
         String precode = scanner.nextLine();
         System.out.println("The input you entered was: "+precode);
-        //Analyze and print out frequency of characters
+        //Analyze frequency of characters
         System.out.println("Analyzing the frequency of the characters in the input...");
         Map<Character, Double> countMap = freqCounter.getCount(precode);
         Map<Character, Double> freqMap = freqCounter.getFrequency(countMap);
         System.out.println("Character frequencies from the input:");
+        //Print out frequency and place characters into Huffman Nodes and add them to priority queue
+        PriorityQueue minHeap = new PriorityQueue();
         for (Map.Entry<Character, Double> freq : freqMap.entrySet()) {
             System.out.println(freq.getKey() + "-> " +freq.getValue());
+            minHeap.add(new HuffmanNode(freq.getKey(), freq.getValue()));
         }
-        //Place characters into Huffman Nodes
-
-
-
     }
 }
